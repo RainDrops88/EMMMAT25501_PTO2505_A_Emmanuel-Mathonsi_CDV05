d86782a0eba0442530edc1dbd9a77d2160439123
Emmanuel <07942jerry@gmail.com>
2026-03-02 23:13:21 +0200

    creating header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>Emmanuel Mathonsi</h1>
+            <ul  className="nav-links">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">My resume</a></li>
+            </ul>
+        </header>
+    )
+}
