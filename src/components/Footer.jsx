65455ecffe85fa7c7d0d09988f36f9e7a3e89278
Emmanuel <07942jerry@gmail.com>
2026-03-02 23:12:18 +0200

    creating a footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer () {
+    return (
+        <footer>
+            <div className="contact">
+                <h3>Contact Me</h3>
+                <p>Email: <a href=""></a></p>
+                <p>Phone: <a href=""></a></p>
+                <p>LinkedIn: <a href=""></a></p>
+                <p>GitHub: <a href=""></a></p>
+            </div>
+            <p>&copy; 2026 Emmanuel Mathonsi. All rights reserved.</p>
+        </footer>
+    )
+}
