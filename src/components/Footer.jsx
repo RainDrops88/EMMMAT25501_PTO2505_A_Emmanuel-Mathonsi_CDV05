e439fe3147486bb03379b12e74ed4aa967c534f1
Emmanuel <07942jerry@gmail.com>
2026-03-03 19:05:50 +0200

    adding links and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,23 @@
+
+
 export default function Footer () {
     return (
         <footer>
             <div className="contact">
-                <h3>Contact Me</h3>
-                <p>Email: <a href=""></a></p>
-                <p>Phone: <a href=""></a></p>
-                <p>LinkedIn: <a href=""></a></p>
-                <p>GitHub: <a href=""></a></p>
+                <span>Email: <a href=""> 07942jerry@gmail.com</a></span>
+                <span>Phone: <a href="">079 428 8962</a></span>
+                <span>
+                    <a href="https://www.linkedin.com/in/emmanuel-mathonsi-300b33308/" target="_blank" rel="noopener noreferrer"> 
+                    <img src="https://custom-icon-badges.demolab.com/badge/LinkedIn-0A66C2?logo=linkedin-white&logoColor=fff" 
+                    alt="LinkedIn profile" />
+                    </a>
+                </span>
+                <span>
+                    <a href="https://github.com/RainDrops88" target="_blank" rel="noopener noreferrer">
+                    <img src="https://custom-icon-badges.demolab.com/badge/GitHub-181717?logo=github-white&logoColor=fff" 
+                    alt="GitHub profile" />
+                    </a>
+                    </span>
             </div>
             <p>&copy; 2026 Emmanuel Mathonsi. All rights reserved.</p>
         </footer>
