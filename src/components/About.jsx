9bf63a8b43de954a3bcd2040014415f34d0dfbfb
Emmanuel <07942jerry@gmail.com>
2026-03-04 19:53:22 +0200

    creating about page/component

---
+++ src/components/About.jsx
@@ -0,0 +1,45 @@
+
+export default function About () {
+    return (
+        <main>
+            <section id="about">
+                <h2>Professional Summay</h2>
+                <p className="p-summary">
+                    I am a passionate and dedicated Software Developer with a strong foundation 
+                    in programming.I have hands-on experience in building dynamic and responsive
+                    websites using various programming languages and frameworks,
+                    I am committed to continuous learning and growth in the field of 
+                    Software Development. My goal is to contribute to impactful projects 
+                    that make a difference in people's lives.
+                </p>
+            </section>
+            <section id="work-experience">
+                <h2>Experience</h2>
+                <div className="experience">
+                    <p className="duration">August 2022 - Present</p>
+                    <h3 className="post">Air Traffic Control TTech Solutions Irainee</h3>
+                    <p className="company">Sout African Air Force</p>
+                    <p className="location">New York, NY</p>
+                    <ul className="responsibilities">
+                        <li>Assisted in developing and maintaining web applications using React and Node.js.</li>
+                        <li>Collaborated with the development team to implement new features and fix bugs.</li>
+                        <li>Participated in code reviews and contributed to improving code quality.</li>
+                        <li>Gained experience in Agile development methodologies and version control using Git.</li>
+                    </ul>
+                </div>
+                <div className="experience">
+                    <p className="duration">April 2019 - August 2022</p>
+                    <h3 className="post">Software Developer Intern</h3>
+                    <p className="company">Tech Solutions Inc.</p>
+                    <p className="location">New York, NY</p>
+                    <ul className="responsibilities">
+                        <li>Assisted in developing and maintaining web applications using React and Node.js.</li>
+                        <li>Collaborated with the development team to implement new features and fix bugs.</li>
+                        <li>Participated in code reviews and contributed to improving code quality.</li>
+                        <li>Gained experience in Agile development methodologies and version control using Git.</li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+    )
+}
