fafd21b59a2c6bae83e93cc5f5be2b154f3d6f9b
Emmanuel <07942jerry@gmail.com>
2026-03-04 19:52:45 +0200

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import React from 'react'
 import Header from './components/Header'
 import Home from './components/Home'
 import './App.css'
 import Footer from './components/Footer'
+import About from './components/About'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      <Home  />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
      
     </>
