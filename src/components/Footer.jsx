c1c72eee4d71b89cd1c1b6db89ab365b3e5745ba
Emmanuel <07942jerry@gmail.com>
2026-03-04 19:54:31 +0200

    dding class names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ export default function Footer () {
     return (
         <footer>
             <div className="contact">
-                <span>Email: <a href=""> 07942jerry@gmail.com</a></span>
-                <span>Phone: <a href="">079 428 8962</a></span>
-                <span>
+                <span>Email:  07942jerry@gmail.com</span>
+                <span className="socials">
                     <a href="https://www.linkedin.com/in/emmanuel-mathonsi-300b33308/" target="_blank" rel="noopener noreferrer"> 
                     <img src="https://custom-icon-badges.demolab.com/badge/LinkedIn-0A66C2?logo=linkedin-white&logoColor=fff" 
                     alt="LinkedIn profile" />
                     </a>
                 </span>
-                <span>
+                <span className="phone">Phone: 079 428 8962</span>
+                <span className="socials">
                     <a href="https://github.com/RainDrops88" target="_blank" rel="noopener noreferrer">
                     <img src="https://custom-icon-badges.demolab.com/badge/GitHub-181717?logo=github-white&logoColor=fff" 
                     alt="GitHub profile" />
