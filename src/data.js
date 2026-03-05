f2ef6f40f32ec626a7f14956107f9843670f5ae5
Emmanuel <07942jerry@gmail.com>
2026-03-05 10:50:05 +0200

    adding data file

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        id: 1,
+        year: 2026,
+        title: "React Podcast Landing Page",
+        technologies: ["HTML", "CSS", "Javascript", "React"],
+        responsibility: "",
+        link: "https://github.com/RainDrops88/EMMMAT25501_PTO2505_A_Emmanuel-Mathonsi_DJS03.git",
+    },
+
+    {
+        id: 2,
+        year: 2026,
+        title: "Web Component Podcast Preview",
+        technologies: ["HTML", "CSS", "Javascript"],
+        responsibility: "",
+        link: "https://github.com/RainDrops88/EMMMAT25501_PTO2505_A_Emmanuel-Mathonsi_DJS02.git",
+    },
+
+    {
+        id: 3,
+        year: 2025,
+        title: "Kanban Task App",
+        technologies: ["HTML", "CSS", "Javascript"],
+        responsibility: "",
+        link: "https://github.com/RainDrops88/EMMMAT25501_PTO2505_A_Emmauel-Mathonsi_JSLPP.git",
+    },
+
+]
