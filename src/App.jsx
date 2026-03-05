c5c2c5fe65f967e37e8be6e58aa68b8ea8cc8bc6
Emmanuel <07942jerry@gmail.com>
2026-03-05 12:08:42 +0200

    fixing imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import About from './components/About'
-import Projects from './components/Projects'
+import ProjectGrid from './components/ProjectGrid'
+import { projects } from './data.js'
 import { Routes, Route } from 'react-router-dom';
+import './styles/App.css'
 
 function App() {
 
@@ -12,12 +14,12 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<ProjectGrid projects={projects} />} />
       </Routes>
       <Footer />
-     
     </>
   )
 }
