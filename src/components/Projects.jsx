b6362b881d3e130ec29951f7b8d6cb75f4a72e01
Emmanuel <07942jerry@gmail.com>
2026-03-05 12:10:00 +0200

    adding project card

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+export default function Project (props){
+    return (
+        
+        <div className="project">
+            <h3 className="year">{props.year}</h3>
+            <h2 className="title">{props.title}</h2>
+            <p className="brief"><strong>Brief: </strong>{props.brief}</p>
+            <p className="techs"><strong>Technologies: </strong>
+            {props.technologies}</p>
+            <p className="responsibilities"><strong>Responsibilities: </strong>{props.responsibilities}</p>
+            <p className="link"><a href={props.link} target="_blank" rel="noopener noreferrer">GitHub link</a></p>
+
+        </div>
+       
+    )
+}
