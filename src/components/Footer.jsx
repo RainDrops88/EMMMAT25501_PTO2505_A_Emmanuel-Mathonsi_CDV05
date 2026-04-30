743c9c777a6a3a236086034007033b588341e7b5
Emmanuel <07942jerry@gmail.com>
2026-04-30 19:26:35 +0200

    adding links to phone and email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,18 @@ export default function Footer () {
     return (
         <footer>
             <div className="contact">
-                <span>Email:  07942jerry@gmail.com</span>
+                <span className="email">
+                    <a href="mailto:07942jerry@gmail.com" alt="email Emmanuel">Email:  07942jerry@gmail.com</a>
+                </span>
                 <span className="socials">
                     <a href="https://www.linkedin.com/in/emmanuel-mathonsi-300b33308/" target="_blank" rel="noopener noreferrer"> 
                     <img src="https://custom-icon-badges.demolab.com/badge/LinkedIn-0A66C2?logo=linkedin-white&logoColor=fff" 
                     alt="LinkedIn profile" />
                     </a>
                 </span>
-                <span className="phone">Phone: 079 428 8962</span>
+                <span className="phone" >
+                    <a href="tel:+27794288962" alt="call Emmanuel"><span className="phone-icon">Phone</span> 079 428 8962</a>
+                </span>
                 <span className="socials">
                     <a href="https://github.com/RainDrops88" target="_blank" rel="noopener noreferrer">
                     <img src="https://custom-icon-badges.demolab.com/badge/GitHub-181717?logo=github-white&logoColor=fff" 
